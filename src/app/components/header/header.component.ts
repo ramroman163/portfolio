2674bdc2d10467f1cf7ac10084b4a38a7b3e5d36
ramroman163 <ramroman163@gmail.com>
2023-12-17 02:19:05 -0300

    Fixed navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+
+
+export class HeaderComponent implements OnInit{
+  
+
+  constructor(){}
+
+  ngOnInit() {  
+    this.screenWidth = window.innerWidth;  
+    this.screenHeight = window.innerHeight; 
+    
+    if(this.screenWidth >= 1336){
+      console.log(this.screenWidth)
+      this.hideNavbar = true
+    }
+  } 
+
+  screenWidth : number = 0;
+  screenHeight : number = 0;
+
   hideNavbar : Boolean = false
+  innerWidth : any;
 
   toggleNavbar() : void {
     this.hideNavbar = !this.hideNavbar
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onresize() {
+    this.innerWidth = window.innerWidth;
+
+    if(this.innerWidth <= 1336){
+      console.log(this.screenWidth)
+      this.hideNavbar = false
+    }
+    if(this.innerWidth >= 1336){
+      console.log(this.screenWidth)
+      this.hideNavbar = true
+    }
   }
 }
